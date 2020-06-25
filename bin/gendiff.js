7ebbbc77cac518ebd5a7f9ecfcf2123b23e84265
Stepan <goodhrommi@gmail.com>
2020-06-25 20:50:09 +0300

    add option and arguments in commander

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,4 +7,9 @@ const program = new commander.Command();
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2, options) => {
+    console.log(`${filepath1} ${filepath2} ${options.format}`);
+  })
   .parse(process.argv);
