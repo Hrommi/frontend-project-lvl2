89890542a78732c7975ca57a76171a972db982a6
Stepan <goodhrommi@gmail.com>
2020-06-25 20:47:46 +0300

    add help with commander

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
-console.log('init');
+import commander from 'commander';
+
+const program = new commander.Command();
+
+program
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  .parse(process.argv);
