e21a70d546c1d07dd8ed468a15623d87ff2eb106
Stepan <goodhrommi@gmail.com>
2020-06-27 17:49:41 +0300

    create and use fixture with expected result

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,5 +1,6 @@
 import { test, expect, beforeAll } from '@jest/globals';
 import path from 'path';
+import fs from 'fs';
 import genDiff from '../src/index.js';
 
 const extensions = ['json', 'yaml', 'ini'];
@@ -8,21 +9,12 @@ const getFixturePath = (name) => path.join(process.cwd(), '__fixtures__', name);
 let expected;
 
 beforeAll(() => {
-  expected = [
-    '{',
-    '    host: hexlet.io',
-    '  + timeout: 20',
-    '  - timeout: 50',
-    '  - proxy: 123.234.53.22',
-    '  - follow: false',
-    '  + verbose: true',
-    '}',
-  ].join('\n');
+  expected = fs.readFileSync(getFixturePath('result.txt'), 'utf-8');
 });
 
 test.each(extensions)('gendiff %s', (extension) => {
   const filepathBefore = getFixturePath(`before.${extension}`);
   const filepathAfter = getFixturePath(`after.${extension}`);
   const diff = genDiff(filepathBefore, filepathAfter);
-  expect(diff).toBe(expected);
+  expect(diff).toBe(expected.trim());
 });
