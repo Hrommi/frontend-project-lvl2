16dcb9382815a006c23038e503a8555e210b8290
Stepan <goodhrommi@gmail.com>
2020-06-27 17:07:23 +0300

    create a comparison of flat yaml files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,6 +2,7 @@ import { test, expect, beforeAll } from '@jest/globals';
 import path from 'path';
 import genDiff from '../src/index.js';
 
+const extensions = ['json', 'yaml'];
 const getFixturePath = (name) => path.join(process.cwd(), '__fixtures__', name);
 
 let expected;
@@ -19,9 +20,9 @@ beforeAll(() => {
   ].join('\n');
 });
 
-test('gendiff json', () => {
-  const filepathBefore = getFixturePath('before.json');
-  const filepathAfter = getFixturePath('after.json');
+test.each(extensions)('gendiff %s', (extension) => {
+  const filepathBefore = getFixturePath(`before.${extension}`);
+  const filepathAfter = getFixturePath(`after.${extension}`);
   const diff = genDiff(filepathBefore, filepathAfter);
   expect(diff).toBe(expected);
 });
--- src/parsers.js
+++ src/parsers.js
@@ -1,5 +1,8 @@
+import yaml from 'js-yaml';
+
 const extensionsMapping = {
   json: JSON.parse,
+  yaml: yaml.safeLoad,
 };
 
 const parse = (data, extension) => extensionsMapping[extension](data);
