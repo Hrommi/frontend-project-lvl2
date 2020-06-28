0272798edc0ac68eb0d4cf1fb0706c0a045ddd76
Stepan <goodhrommi@gmail.com>
2020-06-28 18:34:21 +0300

    update help in bin file

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,7 +8,7 @@ const program = new commander.Command();
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [format]', 'output format', 'stylish')
+  .option('-f, --format [output format]', 'output format: stylish, plain or json', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2, options) => {
     const diff = genDiff(filepath1, filepath2, options.format);
