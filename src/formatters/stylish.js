46cf727556b167c5179650c0986f38eb4b0b7982
Stepan <goodhrommi@gmail.com>
2020-07-01 11:20:46 +0300

    rename padding to indent

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,8 +1,8 @@
 import isObject from '../utils/isObject.js';
 
-const genPadding = (level, char) => {
-  const padding = '    '.repeat(level);
-  return char ? `${padding.slice(2)}${char} ` : padding;
+const genIndent = (level, char) => {
+  const indent = '    '.repeat(level);
+  return char ? `${indent.slice(2)}${char} ` : indent;
 };
 
 const stringify = (value, level) => {
@@ -12,31 +12,31 @@ const stringify = (value, level) => {
   const keys = Object.keys(value);
   return [
     '{',
-    ...keys.map((key) => `${genPadding(1)}${key}: ${stringify(value[key], level + 1)}`),
+    ...keys.map((key) => `${genIndent(1)}${key}: ${stringify(value[key], level + 1)}`),
     '}',
-  ].join(`\n${genPadding(level)}`);
+  ].join(`\n${genIndent(level)}`);
 };
 
 const typesMapping = {
   nested: (item, level, render) => {
-    const padding = genPadding(level);
-    const value = `{\n${render(item.children, level + 1, render)}\n${padding}}`;
-    return `${padding}${item.name}: ${value}`;
+    const indent = genIndent(level);
+    const value = `{\n${render(item.children, level + 1, render)}\n${indent}}`;
+    return `${indent}${item.name}: ${value}`;
   },
   unchanged: (item, level) => {
-    const padding = genPadding(level);
+    const indent = genIndent(level);
     const value = stringify(item.value, level);
-    return `${padding}${item.name}: ${value}`;
+    return `${indent}${item.name}: ${value}`;
   },
   added: (item, level) => {
-    const padding = genPadding(level, '+');
+    const indent = genIndent(level, '+');
     const value = stringify(item.value, level);
-    return `${padding}${item.name}: ${value}`;
+    return `${indent}${item.name}: ${value}`;
   },
   deleted: (item, level) => {
-    const padding = genPadding(level, '-');
+    const indent = genIndent(level, '-');
     const value = stringify(item.value, level);
-    return `${padding}${item.name}: ${value}`;
+    return `${indent}${item.name}: ${value}`;
   },
   changed: (item, level) => {
     const { valueBefore, ...rest } = item;
